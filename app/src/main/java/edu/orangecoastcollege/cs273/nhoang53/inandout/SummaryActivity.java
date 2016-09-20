4f125bfb250ffef64d5e27da90ca57c007a8c438
Nguyen <he_llo1002@yahoo.com>
2016-09-19 20:25:55 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/nhoang53/inandout/SummaryActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs273.nhoang53.inandout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+}
